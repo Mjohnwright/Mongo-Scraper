46222c599f8dbd39f0e004e396f1fb4b5e36a641
Mjohnwright <34377327+Mjohnwright@users.noreply.github.com>
2018-06-12 13:57:35 -0400

    port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = process.env.MONGODB_URI || 3000;
+var PORT = process.env.PORT || 3000;
 
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
