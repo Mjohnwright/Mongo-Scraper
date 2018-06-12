90b2701e26ace7c007409e7ec73c47d1f3f454ce
Mjohnwright <34377327+Mjohnwright@users.noreply.github.com>
2018-06-12 19:19:46 -0400

    post route for note

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -29,19 +29,51 @@ $(".delete").on("click", function () {
     method: "POST",
     url: "/articles/delete/" + thisId
   }).done(function (data) {
-    window.location = "/saved"
   })
 });
 
 // Executes a POST when to save note
+//notes.handlebars page
+$(document).on("click", ".savenote", function () {
+  console.log("save note is clicked");
+  // Grab the id associated with the article from the submit button
+  var thisId = $(this).attr("data-id");
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+      method: "POST",
+      url: "/notes/articles/" + thisId,
+      data: {
+        // Value taken from title input
+        title: $("#titleinput").val(),
+        // Value taken from note textarea
+        body: $("#bodyinput").val()
+      }
+    })
+    // With that done
+    .then(function (data) {
+      // Log the response
+      console.log(data);
+      // Empty the notes section
+      $("#notes").empty();
+          window.location = "/saved"
+
+    });
+
+  // Remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
+
+// Executes a POST when to save note
 $(document).on("click", ".saveNote", function () {
-  console.log("clicked");
+  console.log("sasve note is clicked");
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
 
   $.ajax({
       method: "POST",
-      url: "/saved/" + thisId,
+      url: "notes/articles/" + thisId,
       data: {
         // Value taken from title input
         title: $("#titleinput").val(),
@@ -60,10 +92,12 @@ $(document).on("click", ".saveNote", function () {
   // Also, remove the values entered in the input and textarea for note entry
   $("#titleinput").val("");
   $("#bodyinput").val("");
+  //closes modal and returns to the savedAricles.handlebars page
+  window.location = "/saved"
 });
 
 // Executes the /articles page and list the data in JSON format
-$.getJSON("/articles", function(data) {
+$.getJSON("/articles", function (data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
@@ -72,4 +106,3 @@ $.getJSON("/articles", function(data) {
 });
 
 //
-
